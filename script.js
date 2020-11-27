b6dc55167e680adbc50cb141079364f081236704
Fernando França <fernandofrancaf7@gmail.com>
2020-11-27 15:09:32 -0300

    finalizaçao do jogo

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 /* função para verificar o tamanho da tela */
 
-var altura, largura, vidas;
+var altura, largura;
+var vidas = 1;
+var tempo = 15;
+var criaMosquitoTempo = 1500;
+
+var nivel = window.location.search;
+nivel = nivel.replace('?', '');
+
+if(nivel === 'normal') {
+
+	criaMosquitoTempo = 1500;
+
+} else if (nivel === 'dificil') {
+
+	criaMosquitoTempo = 1000;
+
+} else if (nivel === 'cacador') {
+
+	criaMosquitoTempo = 750;
+
+}
 
 function ajustarTamanhoTela() {
 
@@ -11,6 +31,26 @@ function ajustarTamanhoTela() {
 
 ajustarTamanhoTela();
 
+
+var cronometro = setInterval(function () {
+
+	tempo = tempo - 1;
+	
+
+	if(tempo < 0) {
+
+		clearInterval(cronometro);
+		clearInterval(criaMosquito);
+		window.location.href = 'vitoria.html';
+
+	} else {
+
+		document.getElementById('cronometro').innerHTML = tempo;
+
+	}
+
+}, 2000);
+
 /* cria posições randomicas para a img dentro do tamanho de tela ajustado */
 
 function posicaoRandomica() {
@@ -18,23 +58,19 @@ function posicaoRandomica() {
 	// remover o mosquito anterior, caso exista
 
 	if (document.getElementById('mosquito')) {
-
 		document.getElementById('mosquito').remove();
 
 		if (vidas > 3) {
 
-			alert('Gamer Over');
+			window.location.href = "gameover.html";
 
 		} else {
 
-			var x = document.getElementById('vida' + vidas);
-			x.src = 'img/coracaovazio.png';
-
+			document.getElementById('vida' + vidas).src = 'img/coracaovazio.png';
 			vidas++;
 
 		}
 	}
-
 
 	var posicaoX = Math.floor(Math.random() * largura) - 90;
 	var posicaoY = Math.floor(Math.random() * altura) - 90;
@@ -53,15 +89,14 @@ function posicaoRandomica() {
 	mosquito.style.top = posicaoY + 'px';
 	mosquito.style.position = 'absolute';
 	mosquito.id = 'mosquito';
+	document.body.appendChild(mosquito);
 
 	mosquito.onclick = function() {
 
 		this.remove();
 
 	}
-
-	document.body.appendChild(mosquito);
-
+	
 }
 
 /* função para linkar as classes que determinam o tamanho do mosquito */
